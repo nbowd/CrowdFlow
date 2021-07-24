e92116813d3aa72591cf70eec8a124aead4eee31
nbowd <bowdenn@oregonstate.edu>
2021-07-24 16:25:53 -0700

    Added other half of the ddl/dml queries, cleaned up format a little

--- crowdflow_ddl.sql
+++ crowdflow_ddl.sql
@@ -64,3 +64,85 @@ CREATE TABLE `Tickets` (
 
 INSERT INTO `Tickets` VALUES (14, 84, 10, 'c', 'ccc', 55.00, 'visa', 2, 1, 0, 1), (27, 582, 17, 'a', 'eee', 100.00, 'cash', 10, 10007, 1, 1), (1003, 978, 20, 'd', 'aaa', 37.50, 'amex', 18, 841, 0, 0);
 
+-- Table structure for `Fans`
+
+DROP TABLE IF EXISTS `Fans`;
+
+CREATE TABLE `Fans`(
+  `fanID` INT AUTO_INCREMENT UNIQUE NOT NULL,
+  `firstName` VARCHAR(50) NOT NULL,
+  `lastName` VARCHAR(50) NOT NULL,
+  `gender` VARCHAR(20) NOT NULL,
+  `birthdate` DATE NOT NULL,
+  `phone` VARCHAR(20),
+  `email` VARCHAR(70),
+  `membership` BOOLEAN NOT NULL,
+  `comment` TINYTEXT, 
+  PRIMARY KEY (`fanID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data dump for `Fans`
+
+LOCK TABLES `Fans` WRITE;
+INSERT INTO `Fans` VALUES (524, 'Steve', 'Jackson','Male', '1999/02/16', '123-456-7890', 'scubasteve@gmail.com', 0, 'Backstage access winner from the LIVE life radio contest.'), (319, 'Jenny', 'Adams', 'Female','1980/09/22', '999-867-5309','tutonephone@gmail.com', 1, NULL), (808, 'Kanye', 'West','Male','1977/06/08', NULL, NULL, 1, 'Requires all staff to address him in third person.');
+UNLOCK TABLES;
+
+-- Table structure for `Employees`
+
+DROP TABLE IF EXISTS `Employees`;
+
+CREATE TABLE `Employees` (
+  `employeeID` INT AUTO_INCREMENT UNIQUE NOT NULL,
+  `firstName` VARCHAR(50) NOT NULL,
+  `lastName` VARCHAR(50) NOT NULL,
+  `birthdate` DATE NOT NULL,
+  `startDate` DATE NOT NULL,
+  `phone` VARCHAR(20),
+  `email` VARCHAR(70),
+  PRIMARY KEY (`employeeID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data dump for `Employees`
+
+LOCK TABLES `Employees` WRITE;
+INSERT INTO `Employees` VALUES (212, 'Ted', 'Stewart', '1970/3/14', '2016/06/06', '999-111-5555', 'tedS@crowdflow.com'),(771, 'Natalie', 'Desoto', '1995/08/27', '2019/05/13', NULL, 'natalied@crowdflow.com'), (347, 'Ash', 'Baker', '2000/04/18', '2020/12/02', '818-350-1144', 'ashb@crowdflow.com');
+UNLOCK TABLES;
+
+-- Table structure for `Job_titles`
+
+DROP TABLE IF EXISTS `Job_titles`;
+
+CREATE TABLE `Job_titles` (
+  `jobID` INT AUTO_INCREMENT UNIQUE NOT NULL,
+  `title` VARCHAR(70) NOT NULL,
+  `description` TINYTEXT NOT NULL,
+  PRIMARY KEY (`jobID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data dump for `Job_titles`
+
+LOCK TABLES `Job_titles` WRITE;
+INSERT INTO `Job_titles` VALUES (4, 'Ticket Seller', 'Main point of sale for in person tickets, handles the bulk of guests arriving at the venue'), (5, 'Senior Ticket Seller', 'Handles all VIP and special sales, backup for ticket sellers'), (3, 'Ticket Manager', 'Manages all ticketing employees, provides them with any resources they need to keep the flow moving');
+UNLOCK TABLES;
+
+-- Table structure for `Employee_job_titles`
+
+DROP TABLE IF EXISTS `Employee_job_titles`;
+
+CREATE TABLE `Employee_job_titles` (
+  `jobID` INT NOT NULL,
+  `employeeID` INT NOT NULL,
+  PRIMARY KEY (`jobID`, `employeeID`),
+  FOREIGN KEY `fk_job`(`jobID`)
+  REFERENCES `job_titles`(`jobID`)
+  ON DELETE CASCADE,
+  FOREIGN KEY `fk_employee`(`employeeID`)
+  REFERENCES `Employees`(`employeeID`)
+  ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data dump for `Employee_job_titles`
+
+LOCK TABLES `Employee_job_titles` WRITE;
+INSERT INTO `Employee_job_titles` VALUES ( 3, 212), (5, 771), (4, 347);
+UNLOCK TABLES;
--- crowdflow_dml.sql
+++ crowdflow_dml.sql
@@ -1,4 +1,5 @@
 ------------------------------------------------------------------------------------------------------------------------------------
+----------------  FOR ALL QUERIES BELOW `:` character used to denote inputs from backend ---------------- 
 
 -- get all the ticket data to display on Tickets page
 SELECT ticketID, eventID, seat, row, section, price, paymentMethod, soldByEmployeeID, soldToFanID, isWillcall, isValid FROM Tickets
@@ -13,19 +14,15 @@ SELECT employeeID FROM Employees
 SELECT fanID from Fans 
 
 -- add a new ticket
--- : character used to denote inputs from backend
 INSERT INTO Tickets (eventID, seat, row, section, price, paymentMethod, soldByEmployeeID, soldToFanID, isWillcall, isValid) VALUES (:eventIDAddInput, :seatAddInput, :rowAddInput, :sectionAddInput, :priceAddInput, :paymentMethodAddInput, :soldByEmployeeIDAddInput, :soldToFanIDAddInput, :isWillcallAddInput, :isValidAddInput)
 
 -- get a single ticket's data for the update ticket form
--- : character used to denote inputs from backend
 SELECT eventID, seat, row, section, price, paymentMethod, soldByEmployeeID, soldToFanID, isWillcall, isValid FROM Tickets WHERE ticketID = :ticketIDUpdateInput
 
 -- update ticket based on submission on update ticket form
---: character used to denote inputs from backend
 UPDATE Tickets SET eventID=:eventIDUpdateInput, seat=:seatUpdateInput, row=:rowUpdateInput, section=:sectionUpdateInput, price=:priceUpdateInput, paymentMethod=:paymentMethodUpdateInput, soldByEmployeeID=:soldByEmployeeIDUpdateInput, soldToFanID=:soldToFanIDUpdateInput, isWillcall=:isWillcallUpdateInput, isValid=:isValidUpdateInput WHERE ticketID=:ticketIDInput
 
 -- delete a ticket
--- : character used to denote inputs from backend
 DELETE FROM Tickets WHERE ticketID = :ticketIDDeleteInput
 
 ------------------------------------------------------------------------------------------------------------------------------------
@@ -34,31 +31,24 @@ DELETE FROM Tickets WHERE ticketID = :ticketIDDeleteInput
 SELECT eventID, eventName, musicType, eventDate, eventTime, isCancelled FROM Events
 
 -- add a new event
--- : character used to denote inputs from backend
 INSERT INTO Events (eventName, musicType, eventDate, eventTime, isCancelled) VALUES (:eventNameAddInput, :musicTypeAddInput, :eventDateAddInput, :eventTimeAddInput, :isCancelledAddInput)
 
 -- get a single event's data for the update event form
--- : character used to denote inputs from backend
 SELECT eventName, musicType, eventDate, eventTime, isCancelled WHERE eventID = :eventIDUpdateInput
 
 -- update event based on submission from update event form
--- : character used to denote inputs from backend
 UPDATE Events SET eventName=:eventNameUpdateInput, musicType=:musicTypeUpdateInput, eventDate=:eventDateUpdateInput, eventTime=:eventTimeUpdateInput, isCancelled=:isCancelledUpdateInput WHERE ticketID=:ticketIDUpdateInput
 
 -- delete an event
--- : character used to denote inputs from backend
 DELETE FROM Events WHERE eventID=:eventIDDeleteInput
 
 -- get all events that occurred on a specified date
--- : character used to denote inputs from backend
 SELECT eventID, eventName, musicType, eventDate, eventTime, isCancelled FROM Events WHERE eventDate=:filterDate
 
 -- get all events that occured PRIOR to a specified date
--- : character used to denote inputs from backend
 SELECT eventID, eventName, musicType, eventDate, eventTime, isCancelled FROM Events WHERE eventDate<:filterDate
 
 -- get all events that occur AFTER to a specified date
--- : character used to denote inputs from backend
 SELECT eventID, eventName, musicType, eventDate, eventTime, isCancelled FROM Events WHERE eventDate>:filterDate
 
 ------------------------------------------------------------------------------------------------------------------------------------
@@ -67,5 +57,84 @@ SELECT eventID, eventName, musicType, eventDate, eventTime, isCancelled FROM Eve
 SELECT eventID, fanID FROM Events_fans
 
 -- associate a fan with an event (M:M relationship addition)
---: character used to denote inputs from backend
 INSERT INTO Events_fans (eventID, fanID) VALUES (:eventIDAddInput, :fanIDAddInput)
+
+-------- EMPLOYEE PAGE QUERIES --------
+
+-- Populate page table
+SELECT * FROM employees;
+
+-- Add new employee
+INSERT INTO employees (firstName, lastName, birthdate, startDate, phone, email) VALUES (:firstName, :lastName, :birthdate, :startDate, :phone, :email);
+
+-- Search by employeeID
+SELECT employeeID, firstName, lastName, birthdate, startDate, phone, email FROM employees WHERE employeeID = :employeeID;
+
+-- Search by first name
+SELECT employeeID, firstName, lastName, birthdate, startDate, phone, email FROM employees ORDER BY firstName ASC;
+
+-- Search by last name
+SELECT employeeID, lastName, firstName, birthdate, startDate, phone, email FROM employees ORDER BY lastName ASC;
+
+-- Employee updated via button
+UPDATE employees SET lastName= :lastName, firstName= :firstName, birthdate= :birthdate, startDate= :startDate, phone= :phone, email= :email WHERE employeeID = :employeeID;
+
+-- Employee deleted via button
+DELETE FROM employees WHERE employeeID =:employeeID;
+
+-------- FAN PAGE QUERIES --------
+
+-- Populate page table
+SELECT * FROM fans;
+
+-- Add new fan
+INSERT INTO fans (firstName, lastName, birthdate, gender, phone, email, membership, comment) VALUES (:firstName, :lastName, :birthdate, :gender, :phone, :email, :membership, :comment);
+
+-- Search by fanID
+SELECT fanID, firstName, lastName, birthdate, gender, phone, email, membership, comment FROM fans WHERE fanID = :fanID;
+
+-- Search by Fan First Name
+SELECT fanID, firstName, lastName, birthdate, gender, phone, email, membership, comment FROM fans ORDER BY firstName ASC;
+
+-- Search by Fan Last Name
+SELECT fanID, lastName, firstName, birthdate, gender, phone, email, membership, comment FROM fans ORDER BY lastName ASC;
+
+-- Fan updated from button
+UPDATE fans SET lastName= :lastName, firstName= :firstName, birthdate= :birthdate, gender= :gender, phone= :phone, email= :email, membership= :membership, comment =:comment WHERE fanID= :fanID;
+
+-- Delete fan
+DELETE FROM fans WHERE fanID = :fanID;
+
+-------- JOB TITLES PAGE QUERIES --------
+
+-- Populate page table
+SELECT * FROM job_titles;
+
+-- Add new job title
+INSERT INTO job_titles (jobID, title, description) VALUES (:jobID, :title, :description);
+
+-- Update job from button
+UPDATE job_titles SET title=:title, description=:description WHERE jobID=:jobID;
+
+-- Delete job from button
+DELETE FROM job_titles WHERE jobID=:jobID;
+
+-------- EMPLOYEE JOB TITLES PAGE QUERIES --------
+
+-- Populate page table
+SELECT * FROM employee_job_titles;
+
+-- Add entry
+INSERT INTO employee_job_titles (jobID, employeeID) VALUES (:jobID, :employeeID);
+
+-- Select entry based on jobID
+SELECT jobID, employeeID FROM employee_job_titles WHERE jobID=:jobID
+
+-- Select entry based on employeeID
+SELECT jobID, employeeID FROM employee_job_titles WHERE employeeID=:employeeID
+
+-- Update entry from button
+UPDATE employee_job_titles SET jobID=:jobID, employeeID=:employeeID WHERE jobID=::jobID AND employeeID=::employeeID;
+
+-- Delete entry from button
+DELETE FROM employee_job_titles WHERE jobID=:jobID AND employeeID=:employeeID;
