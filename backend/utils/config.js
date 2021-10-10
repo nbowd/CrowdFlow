2af8a978c703865788a826ca6068650697c9b837
nbowd <69742419+nbowd@users.noreply.github.com>
2021-10-10 14:46:55 -0700

    Update config.js

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,9 +2,9 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'classmysql.engr.oregonstate.edu',
-    user: 'cs340_bowdenn',
-    password: '4590',
+    host: 'host',
+    user: 'user',
+    password: 'password',
     database: 'cs340_bowdenn',
     dateStrings: 'true'  // This allows the date to be in the correct format
 
